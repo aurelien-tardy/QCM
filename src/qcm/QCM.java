6b2583694bd6316076de597dd7415df760a52778
Marimax01 <unconfigured@null.spigotmc.org>
2015-12-08 09:49:50 +0100

    Ajout des attributs, fonctions et procedure ainsi que le constructeur

--- src/qcm/QCM.java
+++ src/qcm/QCM.java
@@ -5,10 +5,43 @@
  */
 package qcm;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author aurélien
  */
-public class Qcm {
+public class QCM {
+    String nom;
+    String tag;
+    ArrayList<Question> tabQuestion;
+    
+    QCM(String nom,String tag){
+        this.nom = nom;
+        this.tag = tag;
+        tabQuestion = new ArrayList();
+    }
+    
+    String getTag(){
+        return this.tag;
+    }
+    
+    void addQuestion(Question q){
+        if(tabQuestion.contains(q) == false)
+            tabQuestion.add(q);
+    }
+    
+    boolean delQuestion(int i){
+        if(tabQuestion.contains(tabQuestion.get(i))){
+            tabQuestion.remove(i);
+            return true;
+        }
+        return false;
+    }
+    
+    void modifQuestion(Question q, int i){
+        if(this.delQuestion(i))
+            tabQuestion.add(i, q);
+    }
     
 }
