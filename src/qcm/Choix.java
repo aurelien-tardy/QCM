2d39ba3a8c4d586b134a480400967553da518c95
Marimax01 <maxime.ducoroy01@gmail.com>
2016-01-05 11:10:16 +0100

    test adresse mail

--- src/qcm/Choix.java
+++ src/qcm/Choix.java
@@ -1,14 +1,6 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package qcm;
 
-/**
- *
- * @author aurélien
- */
 public class Choix {
     private String nom;
     private boolean juste;
